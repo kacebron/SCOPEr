25f489cfd3ca847b61af189afdb9a3618fe502b5
kelbino <71789890+kacebron@users.noreply.github.com>
2023-04-05 11:51:23 -0400

    Update_set_paramFile
    Removed the description for the path

--- R/02_set_paramFile.R
+++ R/02_set_paramFile.R
@@ -1,12 +1,12 @@
 #' Modify set_parameter_filenames.csv
 #'
 #' This function helps you modify set_parameter_filenames.csv input file.
-#' @param path Filepath of the directory where set_parameter_filenames.csv is stored
+#'
 #' @param input Change the name of the .csv file where input data will be grabbed by SCOPE.
 #'
-#' @return **path** Value is a string that represents filepath of the directory where set_parameter_filenames.csv is stored. Default value is "../../SCOPE/set_parameter_filenames.csv" which should work even if you don't specify the filepath. In case it does not, copy and paste the long filepath as a value of this argument.
 #' @return **input** By default, the value of this argument is "input_data_latin_hypercube.csv" which is also the default setting in SCOPE. Change this to "input_data_default.csv" if you want to simulate individual runs or time series experiments. Note: You can also changed the default values inside the input_data_default.csv using the function input_data_default().
 #' @export
+#'
 set_paramFile <- function(input = "input_data_latin_hypercube.csv"){
   path <- "./inst/extdata/set_parameter_filenames.csv"
   set_param <- readr::read_file(path)
